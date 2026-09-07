19b338000fcfd951c50d7b7e6a6418ca2f92644b
romanglitch <tagshowpictures@gmail.com>
2026-09-08 00:54:55 +0300

    Icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ export const metadata: Metadata = {
 		capable: true,
 		statusBarStyle: "default"
 	},
-	manifest: "/manifest.json",
 	icons: {
 		icon: "/favicon.ico",
 	},
@@ -20,20 +19,27 @@ export const viewport: Viewport = {
 	maximumScale: 1,
 	userScalable: false,
 	viewportFit: "cover",
+	themeColor: [
+		{media: "(prefers-color-scheme: light)", color: "white"},
+		{media: "(prefers-color-scheme: dark)", color: "black"},
+	],
 };
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
 	return (
 		<html lang="ru" suppressHydrationWarning>
 		<head>
+			<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96"/>
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+			<link rel="manifest" href="/site.webmanifest"/>
+			<meta name="apple-mobile-web-app-title" content="PM"/>
 			<meta name="mobile-web-app-capable" content="yes"/>
 			<meta name="apple-mobile-web-app-capable" content="yes"/>
-			<meta name="apple-mobile-web-app-title" content="PM"/>
-			<meta name="theme-color" content="#f9fafb" media="(prefers-color-scheme: light)"/>
-			<meta name="theme-color" content="#030712" media="(prefers-color-scheme: dark)"/>
 		</head>
-		<body className="antialiased font-sans bg-gray-50 dark:bg-gray-950 text-gray-900 dark:text-white overflow-hidden	">
-			{children}
+		<body
+			className="antialiased font-sans bg-gray-50 dark:bg-gray-950 text-gray-900 dark:text-white overflow-hidden	">
+		{children}
 		</body>
 		</html>
 	);
